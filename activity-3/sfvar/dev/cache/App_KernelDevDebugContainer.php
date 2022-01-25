aacd30f574bd48199bed70863165da0ae9595d50
Everson Araújo <=>
2022-01-25 10:06:11 -0300

    fix gitignore location

--- activity-3/sfvar/dev/cache/App_KernelDevDebugContainer.php
+++ activity-3/sfvar/dev/cache/App_KernelDevDebugContainer.php
@@ -16,6 +16,6 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerRNyYpyr\App_KernelDevDebugContainer([
     'container.build_hash' => 'RNyYpyr',
-    'container.build_id' => '38d6f2d1',
-    'container.build_time' => 1643089403,
+    'container.build_id' => 'df21702a',
+    'container.build_time' => 1643115936,
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerRNyYpyr');
